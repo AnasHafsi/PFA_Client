3aa39ef425c6c5a93e59940ba132fcbc64246c2a
Anas Hafsi <anashafsii@gmail.com>
2019-06-01 14:03:27 +0100

    Added Start & End support

--- app/src/main/java/com/anas/pfav31/MainActivity.java
+++ app/src/main/java/com/anas/pfav31/MainActivity.java
@@ -12,20 +12,25 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
-    public String sendStr ="";
+    public String sendStr = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void lance(View view) {
         EditText txt = findViewById(R.id.txt);
         final String str = txt.getText().toString();
         EditText ip = findViewById(R.id.ip);
-        final String ip_a =ip.getText().toString();
+        final String ip_a = ip.getText().toString();
         EditText sz = findViewById(R.id.size);
         final int size = Integer.parseInt(String.valueOf(sz.getText()));
-
+        EditText end = findViewById(R.id.end);
+        final String str_end = end.getText().toString();
+        EditText start = findViewById(R.id.start);
+        final String str_start = start.getText().toString();
         Thread th = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -33,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     Socket sc = new Socket(ip_a, 666);
                     BufferedWriter writer = new BufferedWriter(
                             new OutputStreamWriter(sc.getOutputStream()));
-                    sendStr=size + ";" +str;
-                    //0000100000000010000000001000000000100000000010000000000000000000100000000010000000001000000000000000
+                    sendStr = size + ";" + str + ";" + str_start + ";" + str_end;
                     writer.write(sendStr);
                     writer.flush();
                     writer.close();
